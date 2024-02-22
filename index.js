9640fb6d3133b9aa5768cca52cac05d235d2b03f
rajkumardevlove <78945346+rajkumardevlove@users.noreply.github.com>
2024-02-22 17:33:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,82 @@
 const express = require('express');
-const { GoogleGenerativeAI } = require("@google/generative-ai");
+const {
+  GoogleGenerativeAI,
+  HarmCategory,
+  HarmBlockThreshold,
+} = require("@google/generative-ai");
+require('dotenv').config();
 
+const cors = require('cors');
 const app = express();
 const port = 5000;
 
 app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send("Hello World");
+});
 
 app.post('/generateContent', async (req, res) => {
   try {
-  const API_KEY = 'AIzaSyDoeQSysD3ad-oSuCyCmdwuXrVYkR8wah8';
-  const genAI = new GoogleGenerativeAI(API_KEY);
-  const model = genAI.getGenerativeModel({ model: "gemini-pro"});
-  const {prompt} = req.body;
-  const result = await model.generateContent(prompt);
-  const response = await result.response;
-  const text = response.text();
-  return res.json({ text });
+    const API_KEY = process.env.API_KEY;
+    const genAI = new GoogleGenerativeAI(API_KEY);
+    const model = genAI.getGenerativeModel({ model: "gemini-pro"});
+    const {prompt} = req.body;
+
+    const generationConfig = {
+      temperature: 0.9,
+      topK: 1,
+      topP: 1,
+      maxOutputTokens: 2048,
+    };
+
+    const safetySettings = [
+      {
+        category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+        threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+      },
+      {
+        category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+        threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+      },
+      {
+        category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+        threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+      },
+      {
+        category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+        threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+      },
+    ];
+
+    const chat = model.startChat({
+      generationConfig,
+      safetySettings,
+      history: [
+        {
+          role: "user",
+          parts: [{ text: "How are you ?"}],
+        },
+        {
+          role: "model",
+          parts: [{ text: "As an AI, I don't have personal feelings or emotions, so I don't experience subjective states like happiness, sadness, or boredom. My primary function is to assist and provide information to users. How can I help you today?"}],
+        },
+      ],
+    });
+  
+    const result = await chat.sendMessage(prompt);
+    if (!result) {
+      throw new Error('Failed to generate content');
+    }
+    const response = await result.response;
+    if (!response) {
+      throw new Error('Failed to get response');
+    }
+    const text = response.text();
+    return res.json({ text });
   } catch (error) {
+    console.error(error);
     res.status(500).json({ error: error.message});
   }
 });
